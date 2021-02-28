df89e29c274ee44d0333078b75adba7b55bcbef3
alk3mist <alexandr.gavrilin@hotmail.com>
2021-02-28 18:14:10 +0200

    💩 version

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
           <Link to={'/'}>Home</Link> {'  '}
           {/*<Link to={'/otherpage'}>Other Page</Link>*/}
         </header>
-        <h1>Инна Какаська!</h1>
+        <h1>Инна - Какаська 💩!</h1>
         <hr/>
         {/*<div>*/}
         {/*  <Route exact={true} path={'/'} component={Fib}/>*/}
